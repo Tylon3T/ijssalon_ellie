f554c1163de1b9348d5b90fbb705b098229bf26f
Tylon3T <69208677+Tylon3T@users.noreply.github.com>
2023-09-28 11:14:40 +0200

    Huiswerk les 8 gemaakt

---
+++ algemene_functies.py
@@ -0,0 +1,11 @@
+def mijn_functie_1(a):
+    b = a * a
+    return b
+
+def mijn_functie_2(a,b):
+    c = a + b
+    d = a - b
+    e = a * b
+    f = a / b
+    g = [c,d,e,f]
+    return g
---
+++ reclame.py
@@ -0,0 +1,29 @@
+from algemene_functies import mijn_functie_2
+
+def aanbieding_1(smaak,prijs,korting):
+    korting = prijs-(prijs/100)*(korting*100)
+    return f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {korting} euro."
+
+def inkomsten_totaal(inkomsten,btw):
+    inkomsten = sum(inkomsten)
+    btw_bedrag = (inkomsten/100)*(btw*100)
+    return f"Het totaal van alle inkomsten van deze week is {inkomsten} euro, waarover {btw_bedrag} euro btw betaald dient te worden."
+
+def hoog_en_laag(mijn_lijst):
+    a=max(mijn_lijst)
+    b=min(mijn_lijst)
+    max_en_min=[a,b]
+    return max_en_min
+
+def gemiddelde(mijn_lijst):
+    a=sum(mijn_lijst)
+    b=len(mijn_lijst)
+    x=a/b
+    return f"De gemiddelde inkomsten deze week zijn {x} euro."
+
+def meervoudig(invoer_lijst):
+    return hoog_en_laag(invoer_lijst)
+
+def combinatie(invoer_lijst_2):
+    korte_lijst=meervoudig(invoer_lijst_2)
+    return mijn_functie_2(korte_lijst[0],korte_lijst[1])
