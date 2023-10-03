daef4dc4320f0bca8140488ddb0e65d84de2cec1
Tylon3T <69208677+Tylon3T@users.noreply.github.com>
2023-10-03 11:09:22 +0200

    Huiswerk les 8 gemaakt

--- tijdelijk.py
+++ tijdelijk.py
@@ -6,11 +6,13 @@ def print_aanbieding():
         "vanille":4,
         "chocolade":5,
     }
-    aanbieding=prijzen["vanille"]*0.8
+    aanbieding=prijzen["aardbei"]*0.8
 
-    reclame_tekst=f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}"
+    reclame_tekst=f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts € {aanbieding}"
 
-    reclame_tekst3=(reclame_tekst).upper()
+    reclame_tekst2 = reclame_tekst[:63]
+
+    reclame_tekst3=(reclame_tekst2).upper()
 
     reclame_tekst4=reclame_tekst3.split(" ")
 
